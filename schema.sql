b14ba401aaa06b1814b47d744f468e870f07bcb6
Tshakalisa Khupe <tkhupe@gmail.com>
2023-05-04 18:31:26 -0500

    organized tables

--- schema.sql
+++ schema.sql
@@ -1,9 +1,11 @@
-CREATE DATABASE hr_records;
-USE hr_records_db,
+DROP DATABASE IF EXISTS bells_hr_records;
+CREATE DATABASE bells_hr_records;
+USE bells_hr_records;
 CREATE TABLE department (
     department_name VARCHAR(30) NOT NULL,
     department_id INT PRIMARY KEY
-)
+);
+
 CREATE TABLE role (
    
     role_name VARCHAR(30),
@@ -11,11 +13,11 @@ CREATE TABLE role (
     salary DECIMAL(5),
     department_id INT NOT NULL
     
-),
+);
 CREATE TABLE employee ( 
     first_name VARCHAR(30),
     last_name VARCHAR(30),
-    employee_id INT,
+    employee_id INT NOT NULL,
     manager VARCHAR(30),
     manager_id INT,
     employee_current BOOLEAN DEFAULT FALSE
